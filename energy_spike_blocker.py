c82f1a0dcb000619012fee6d7a6ba3c7a1ac532a
Dag Frode <tncditto@gmail.com>
2016-03-09 22:30:17 +0100

    Updated code to fit the requirements

--- energy_spike_blocker.py
+++ energy_spike_blocker.py
@@ -4,65 +4,73 @@ import os
 
 from flask import Flask, request, render_template
 
-APPARATER_FIL_NAVN = os.path.dirname(os.path.realpath(__file__)) + '/apparater.json'
+SETTINGS_FIL_NAVN = os.path.dirname(os.path.realpath(__file__)) + '/settings.json'
+STATUS_FIL_NAVN = os.path.dirname(os.path.realpath(__file__)) + '/status.json'
+
 STATUSER =[u"av", u"på", u"auto"]
-PROSENTER=[50, 60, 70, 80, 90, 100]
-APPARAT_MAL = {
-                "apparat_navn": "",
-                "apparat_beskrivelse": "",
-                "apparat_slaes_av_ved": "",
-                "apparat_status": "",
-            }
+PROSENTER=[70, 75, 80, 85, 90, 100]
+SETTINGS_MAL = {
+    "navn": "",
+    "id": "",
+    "trigg": "",
+    "modus": "",
+}
+
+STATUS_MAL = {
+    "status": "",
+    "forbruk": ""
+}
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    stromforbruk_i_prosent = 40
-    stromforbruk_i_kw = 123
+def main():
 
     if request.method == 'POST':
-        apparat_navn = request.form.getlist("apparat_navn")
-        apparat_beskrivelse = request.form.getlist("apparat_beskrivelse")
-        apparat_status = request.form.getlist("apparat_status")
-        apparat_slaes_av_ved = request.form.getlist("apparat_slaes_av_ved")
-        print(apparat_navn)
-        print(apparat_beskrivelse)
-        print(apparat_status)
-        print(apparat_slaes_av_ved)
+        navn = request.form.getlist("navn")
+        ids = request.form.getlist("id")
+        modus= request.form.getlist("modus")
+        trigg= request.form.getlist("trigg")
 
         apparater = []
-        for i in range(0, len(apparat_navn)):
-            if apparat_navn[i]:
+        for i in range(0, len(navn)):
+            if navn[i]:
                 apparat = {
-                            "navn": apparat_navn[i],
-                            "beskrivelse": apparat_beskrivelse[i],
-                            "status": apparat_status[i],
-                            "slaes_av_ved": int(apparat_slaes_av_ved[i]),
-                        }
+                    "navn": navn[i],
+                    "id": ids[i],
+                    "modus": modus[i],
+                    "trigg": int(trigg[i]),
+                }
                 apparater.append(apparat)
 
-        with open(APPARATER_FIL_NAVN , 'w') as outfile:
+        with open(STATUS_FIL_NAVN , 'w') as outfile:
             json.dump({"apparater":apparater}, outfile, sort_keys=True, indent=4, separators=(',', ': '))
 
     try:
-        with open(APPARATER_FIL_NAVN) as data_file:
+        with open(STATUS_FIL_NAVN) as data_file:
             data = json.load(data_file)
             apparater = data.get("apparater")
     except:
-        with open(APPARATER_FIL_NAVN , 'w') as outfile:
+        with open(STATUS_FIL_NAVN , 'w') as outfile:
             json.dump({"apparater":[]}, outfile)
-        with open(APPARATER_FIL_NAVN ) as data_file:
+        with open(STATUS_FIL_NAVN ) as data_file:
             data = json.load(data_file)
             apparater =  data.get("apparater")
 
-    apparater.append(APPARAT_MAL)
+    try:
+        with open(STATUS_FIL_NAVN) as data_file:
+            data = json.load(data_file)
+            status = data.get("status")
+            forbruk = data.get("forbruk")
+    except:
+        print(STATUS_FIL_NAVN + "not found")
 
-    return render_template('index.html', statuser=STATUSER, prosenter=PROSENTER, apparater=apparater, stromforbruk_i_prosent=stromforbruk_i_prosent, stromforbruk_i_kw=stromforbruk_i_kw)
+
+    return render_template('index.html', statuser=STATUSER, prosenter=PROSENTER, apparater=apparater, status=status , forbruk=forbruk )
 
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
